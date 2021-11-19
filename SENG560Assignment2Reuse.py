9e761b387d669afc4dfac9bfde6841a2f9bfc06b
adtomaro72 <90587289+adtomaro72@users.noreply.github.com>
2021-11-19 02:46:00 -0500

    Create SENG560Assignment2Reuse.py

---
+++ SENG560Assignment2Reuse.py
@@ -0,0 +1,15 @@
+import reusable_arithmetic
+
+print("Using the reusable_arithmetic python library!")
+
+print("Adding 2 + 2")
+print(reusable_arithmetic.add(2, 2));
+
+print("Subtracting 2 - 2")
+print(reusable_arithmetic.subtract(2, 2));
+
+print("Multiplying 2 * 2")
+print(reusable_arithmetic.multiply(2, 2));
+
+print("Dividing 4 / 2")
+print(reusable_arithmetic.divide(4, 2));
